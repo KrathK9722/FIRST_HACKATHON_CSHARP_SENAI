efd6d6e2fa687f2d44e8f5472069b5054cf6a76c
Fernando797dev <fernando.mai.dubiella@gmail.com>
2026-09-16 21:10:48 -0300

    Commit com o card vision instacionando

---
+++ Desafio_CRUD/CardVision.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Desafio_CRUD
+{
+    public partial class CardVision : UserControl
+    {
+        // 1. Registro da propriedade do Título
+        public static readonly DependencyProperty CardTitleProperty =
+            DependencyProperty.Register(
+                nameof(CardTitle),
+                typeof(string),
+                typeof(CardVision),
+                new PropertyMetadata("Título Padrão"));
+
+        public string CardTitle
+        {
+            get => (string)GetValue(CardTitleProperty);
+            set => SetValue(CardTitleProperty, value);
+        }
+
+        // 2. Registro da propriedade da Descrição
+        public static readonly DependencyProperty CardDescriptionProperty =
+            DependencyProperty.Register(
+                nameof(CardDescription),
+                typeof(string),
+                typeof(CardVision),
+                new PropertyMetadata("Descrição padrão do card."));
+
+        public string CardDescription
+        {
+            get => (string)GetValue(CardDescriptionProperty);
+            set => SetValue(CardDescriptionProperty, value);
+        }
+
+        // 3. Registro da propriedade da Imagem
+        public static readonly DependencyProperty CardImageSourceProperty =
+            DependencyProperty.Register(
+                nameof(CardImageSource),
+                typeof(string),
+                typeof(CardVision),
+                new PropertyMetadata("https://via.placeholder.com/150"));
+
+        public string CardImageSource
+        {
+            get => (string)GetValue(CardImageSourceProperty);
+            set => SetValue(CardImageSourceProperty, value);
+        }
+
+        public CardVision()
+        {
+            InitializeComponent();
+        }
+
+        private void Card_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Card '{CardTitle}' foi clicado!");
+            e.Handled = true;
+        }
+    }
+}
